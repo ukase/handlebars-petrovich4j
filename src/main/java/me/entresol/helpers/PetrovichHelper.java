68f986772618fe986f37f0547536c7220ee98fa6
Pavel Uvarov <pauknone@yahoo.com>
2015-12-24 15:43:36 +0300

    Initial commit for Petrovich helper

--- src/main/java/me/entresol/helpers/PetrovichHelper.java
+++ src/main/java/me/entresol/helpers/PetrovichHelper.java
@@ -1,7 +1,33 @@
+/*
+ * Copyright (c) 2015 Konstantin Lepa <konstantin+ukase@lepabox.net>
+ *
+ * This file is part of Handlebars Helpers.
+ *
+ * Ukase is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License 2 as
+ * published by the Free Software Foundation, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Apache License 2 for more details.
+ *
+ * You should have received a copy of the Apache License 2
+ * along with this program. If not, see <http://www.apache.org/licenses/>.
+ */
+
 package me.entresol.helpers;
 
-/**
- * Created by puvarov on 24.12.2015.
- */
-public class PetrovichHelper {
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class PetrovichHelper implements Helper<Map<String, Object>> {
+    @Override
+    public CharSequence apply(Map<String, Object> context, Options options) throws IOException {
+        return null;
+    }
 }
